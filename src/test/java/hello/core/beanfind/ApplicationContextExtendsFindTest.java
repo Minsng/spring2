46d7a7b2640b8ef2d1b51f7b506a82d12e9da932
minsng <tjdalstmd033@naver.com>
2023-05-19 11:18:35 +0900

    0519

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -27,8 +27,9 @@ public class ApplicationContextExtendsFindTest {
     @Test
     @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 빈 이름을 지정하면 된다")
     void findBeanByParentTypeBeanName() {
-        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
-        assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+//        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
+        assertThrows(NoUniqueBeanDefinitionException.class, () ->
+                ac.getBean(DiscountPolicy.class));
     }
     @Test
     @DisplayName("특정 하위 타입으로 조회")
